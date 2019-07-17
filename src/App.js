723ee51411af26557d1b6616539e93253de651bc
mariprasad <mramakrishna@crain.com>
2019-07-18 02:00:08 +0530

    add project source code: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone'
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import './App.css';
+import DocumentIcon from './assets/documentIcon.png';
+import AppIcon from './assets/logo__sm--white.png';
+import uploadIcon from './assets/upload__icon.png'
 
 function App() {
+  const [uploadedFiles, addToUploadfiles] = useState([])
+  const [selectedDocument, updateSelectedDocument] = useState({})
+  const onDrop = acceptedFiles => {
+    addToUploadfiles([...uploadedFiles, ...acceptedFiles])
+  }
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="pdfUploadSection">
+      <div><img className="logo__sm--white" src={AppIcon}></img></div>
+      <div className="section__title">FILES</div>
+        {Array.isArray(uploadedFiles) && uploadedFiles.map((file, index) => {
+            const documentNumber = ('0' + (index + 1)).slice(-2)
+            return (
+              <div className="uploadSectionFiles" onClick={() => updateSelectedDocument({file, documentNumber})}>
+                <div><img src={DocumentIcon}></img></div>
+                <div className="fileName__container">
+                   <div className="fileName--title">Document {documentNumber}</div>
+                   <div className="fileName--subtitle">Nam vel porta velit</div>
+                </div>
+               
+              </div>
+            )
+          })}
+        <div className="uploadButtonContainer">
+          <div {...getRootProps()} className="dropzoneHelper">
+            <input {...getInputProps()} />
+            <button className="uploadButton"><img src={uploadIcon}></img> Upload Files</button>
+          </div>
+        </div>
+      </div>
+      <div className="pdfViewSection">
+        {selectedDocument.documentNumber && (`Document ${selectedDocument.documentNumber}`)}
+      </div>
     </div>
   );
 }
